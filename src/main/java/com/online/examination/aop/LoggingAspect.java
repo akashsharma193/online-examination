76375444e66a0c5ae6f226cd749d2f8900fc7aab
kshsa <akash.s@unotechsoft.com>
2025-02-08 18:31:40 +0530

    added aop

--- src/main/java/com/online/examination/aop/LoggingAspect.java
+++ src/main/java/com/online/examination/aop/LoggingAspect.java
@@ -3,9 +3,18 @@ package com.online.examination.aop;
 import java.util.Arrays;
 
 import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.online.examination.dto.UserDto;
+import com.online.examination.response.Response;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -24,6 +33,16 @@ public class LoggingAspect {
 	public void beforeServiceAdvice(JoinPoint joinPoint) {
 		log.info("Inside service Method :::" + joinPoint.getSignature().toShortString());
 		log.info("Service Payload :::" + Arrays.asList(joinPoint.getArgs()));
+	}
+
+	@Pointcut("execution(* com.online.examination.controller..*(..))")
+	public void controllerMethods() {
+	}
+
+	@AfterReturning(pointcut = "controllerMethods()", returning = "result")
+	public void logControllerResponse(Object result) {
+		ResponseEntity<Response> responseEntity = (ResponseEntity<Response>) result;
+		log.info("Controller response: " + responseEntity.getBody());
 	}
 
 }
