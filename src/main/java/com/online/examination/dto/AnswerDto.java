a6d4b87a1e7ea5591b61c62ef55191e09c941f04
kshsa <akash.s@unotechsoft.com>
2025-02-10 15:01:15 +0530

    added correct answer with question

---
+++ src/main/java/com/online/examination/dto/AnswerDto.java
@@ -0,0 +1,22 @@
+package com.online.examination.dto;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Map;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.ToString;
+import lombok.experimental.FieldDefaults;
+
+@Data
+@ToString
+@FieldDefaults(level = AccessLevel.PRIVATE)
+public class AnswerDto {
+
+	List<String> option;
+	
+	String correctAnswer;
+}
