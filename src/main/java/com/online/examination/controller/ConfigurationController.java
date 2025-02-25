7a81c3ed40ada6731c265baf9aa1e1c822f5dafc
kshsa <akash.s@unotechsoft.com>
2025-02-26 00:15:10 +0530

    change file name

--- src/main/java/com/online/examination/controller/ConfigurationController.java
+++ src/main/java/com/online/examination/controller/ConfigurationController.java
@@ -2,37 +2,38 @@ package com.online.examination.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.online.examination.entity.ComplianceEntity;
+import com.online.examination.entity.Configuration;
 import com.online.examination.response.Response;
-import com.online.examination.service.ComplianceService;
+import com.online.examination.service.ConfigurationService;
 
-
+@CrossOrigin
 @RestController
-@RequestMapping("compliance")
+@RequestMapping("configuration")
 public class ConfigurationController {
 
 	@Autowired
-	private ComplianceService complianceService;
+	private ConfigurationService configurationService;
 	
-	@PostMapping("saveCompliance")
-	public ResponseEntity<Response> saveCompliance(@RequestBody ComplianceEntity dto) {
+	@PostMapping("saveConfiguration")
+	public ResponseEntity<Response> saveConfiguration(@RequestBody Configuration dto) {
 		Response response = new Response();
 		response.succeed();
-		response.setData(complianceService.saveCompliance(dto));
+		response.setData(configurationService.saveUser(dto));
 		return ResponseEntity.ok().body(response);
 	}
 	
-	@GetMapping("getCompliance")
-	public ResponseEntity<Response> getCompliance() {
+	@GetMapping("getConfiguration")
+	public ResponseEntity<Response> getConfiguration() {
 		Response response = new Response();
 		response.succeed();
-		response.setData(complianceService.getCompliance());
+		response.setData(configurationService.getConfiguration());
 		return ResponseEntity.ok().body(response);
 	}
 }
