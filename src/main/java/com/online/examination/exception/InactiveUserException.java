a1cecdf4d1432545d81c8fc41feb5ce8b411fac3
kshsa <akash.s@unotechsoft.com>
2025-03-09 19:46:58 +0530

    error code fix

--- src/main/java/com/online/examination/exception/InactiveUserException.java
+++ src/main/java/com/online/examination/exception/InactiveUserException.java
@@ -6,9 +6,17 @@ import com.online.examination.enums.ErrorCode;
 
 
 public class InactiveUserException extends CustomException{
+	
+	public final String message;
 
 	public InactiveUserException() {
 		super(ErrorCode.INACTIVE_USER, HttpStatus.BAD_REQUEST);
+		this.message = "";
+	}
+	
+	public InactiveUserException(String message) {
+		super(ErrorCode.INACTIVE_USER, HttpStatus.BAD_REQUEST, message);
+		this.message = message;
 	}
 
 }
--- src/main/java/com/online/examination/exception/UserAlredayLoggedInException.java
+++ src/main/java/com/online/examination/exception/UserAlredayLoggedInException.java
@@ -6,9 +6,17 @@ import com.online.examination.enums.ErrorCode;
 
 
 public class UserAlredayLoggedInException extends CustomException{
+	
+	public final String message;
 
 	public UserAlredayLoggedInException() {
 		super(ErrorCode.USER_ALREADY_LOGGEDIN, HttpStatus.BAD_REQUEST);
+		this.message="";
+	}
+	
+	public UserAlredayLoggedInException(String message) {
+		super(ErrorCode.USER_ALREADY_LOGGEDIN, HttpStatus.BAD_REQUEST);
+		this.message=message;
 	}
 
 }
